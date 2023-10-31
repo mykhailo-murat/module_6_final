0188c71132f58e398c082e79e8acdc2381a9c615
mykhailo.murat <mykhailo.murat@gmail.com>
2023-10-31 14:53:22 +0200

    +extensions +handler 4 empty folders fixed media and archive handle fn

--- file_parser.py
+++ file_parser.py
@@ -1,11 +1,25 @@
 import sys
 from pathlib import Path
+import os
 
 JPEG_IMAGES = []
 JPG_IMAGES = []
 PNG_IMAGES = []
 SVG_IMAGES = []
+AVI_VIDEO = []
+MP4_VIDEO = []
+MKV_VIDEO = []
+MOV_VIDEO = []
+DOC_DOCUMENTS = []
+DOCX_DOCUMENTS = []
+PDF_DOCUMENTS = []
+TXT_DOCUMENTS = []
+XLSX_DOCUMENTS = []
+PPTX_DOCUMENTS = []
 MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
 MY_OTHER = []
 ARCHIVES = []
 
@@ -14,8 +28,23 @@ REGISTER_EXTENSION = {
     'JPG': JPG_IMAGES,
     'PNG': PNG_IMAGES,
     'SVG': SVG_IMAGES,
+    'AVI': AVI_VIDEO,
+    'MP4': MP4_VIDEO,
+    'MKV': MKV_VIDEO,
+    'MOV': MOV_VIDEO,
+    'DOC': DOC_DOCUMENTS,
+    'DOCX': DOCX_DOCUMENTS,
+    'PDF': PDF_DOCUMENTS,
+    'TXT': TXT_DOCUMENTS,
+    'XLSX': XLSX_DOCUMENTS,
+    'PPTX': PPTX_DOCUMENTS,
     'MP3': MP3_AUDIO,
+    'OGG': OGG_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
     'ZIP': ARCHIVES,
+    'GZ': ARCHIVES,
+    'TAR': ARCHIVES,
 }
 
 FOLDERS = []
@@ -40,11 +69,15 @@ def scan(folder: Path):
             MY_OTHER.append(full_name)
         else:
             try:
-                REGISTER_EXTENSION[extension]
+                ext_reg = REGISTER_EXTENSION[extension]
+                ext_reg.append(full_name)
                 EXTENSIONS.add(extension)
             except KeyError:
                 UNKNOWN.add(extension)  # .mp4, .mov, .avi
                 MY_OTHER.append(full_name)
+
+def is_empty_directory(directory):
+    return not any(os.listdir(directory))
                 
 if __name__ == '__main__':
     folder_process = sys.argv[1]
--- main.py
+++ main.py
@@ -3,34 +3,79 @@ import shutil
 import sys
 import file_parser
 from normalize import normalize
+import os
+
+def is_empty_directory(directory):
+    return not any(os.listdir(directory))
+
+def remove_directory_contents(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            os.remove(file_path)
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            os.rmdir(dir_path)
 
 def handle_media(file_name: Path, target_folder: Path):
-    target_folder.mkdir(exist_ok=True, parents=True)
-    file_name.replace(target_folder / normalize(file_name.name))
+    try:
+        target_folder.mkdir(parents=True, exist_ok=True)
+        file_name.rename(target_folder / normalize(file_name.name))
+    except Exception as e:
+        print(f"Error handling media file: {e}")
 
 def handle_archive(file_name: Path, target_folder: Path):
-    target_folder.mkdir(exist_ok=True, parents=True)
-    folder_for_file = target_folder / normalize(file_name.name.replace(file_name.suffix, ''))
-    folder_for_file.mkdir(exist_ok=True, parents=True)
     try:
+        target_folder.mkdir(parents=True, exist_ok=True)
+        folder_for_file = target_folder / normalize(file_name.stem)
+        folder_for_file.mkdir(parents=True, exist_ok=True)
         shutil.unpack_archive(str(file_name.absolute()), str(folder_for_file.absolute()))
+        file_name.unlink()
     except shutil.ReadError:
         folder_for_file.rmdir()
-        return
-    file_name.unlink()
+    except Exception as e:
+        print(f"Error handling archive: {e}")
 
 def main(folder: Path):
     file_parser.scan(folder)
+
     for file in file_parser.JPEG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPEG')
+        handle_media(file, folder / 'IMAGES' / 'JPEG')
     for file in file_parser.JPG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPG')
+        handle_media(file, folder / 'IMAGES' / 'JPG')
     for file in file_parser.PNG_IMAGES:
-        handle_media(file, folder / 'images' / 'PNG')
+        handle_media(file, folder / 'IMAGES' / 'PNG')
     for file in file_parser.SVG_IMAGES:
-        handle_media(file, folder / 'images' / 'SVG')
+        handle_media(file, folder / 'IMAGES' / 'SVG')
+    for file in file_parser.AVI_VIDEO:
+        handle_media(file, folder / 'VIDEO' / 'AVI')
+    for file in file_parser.MP4_VIDEO:
+        handle_media(file, folder / 'VIDEO' / 'MP4')
+    for file in file_parser.MOV_VIDEO:
+        handle_media(file, folder / 'VIDEO' / 'MOV')
+    for file in file_parser.MKV_VIDEO:
+        handle_media(file, folder / 'VIDEO' / 'MKV')
     for file in file_parser.MP3_AUDIO:
-        handle_media(file, folder / 'audio' / 'MP3_AUDIO')
+        handle_media(file, folder / 'AUDIO' / 'MP3')
+    for file in file_parser.OGG_AUDIO:
+        handle_media(file, folder / 'AUDIO' / 'OGG')
+    for file in file_parser.WAV_AUDIO:
+        handle_media(file, folder / 'AUDIO' / 'WAV')
+    for file in file_parser.AMR_AUDIO:
+        handle_media(file, folder / 'AUDIO' / 'AMR')
+    for file in file_parser.DOC_DOCUMENTS:
+        handle_media(file, folder / 'DOCUMENTS' / 'DOC')
+    for file in file_parser.DOCX_DOCUMENTS:
+        handle_media(file, folder / 'DOCUMENTS' / 'DOCX')
+    for file in file_parser.TXT_DOCUMENTS:
+        handle_media(file, folder / 'DOCUMENTS' / 'TXT')
+    for file in file_parser.XLSX_DOCUMENTS:
+        handle_media(file, folder / 'DOCUMENTS' / 'XLSX')
+    for file in file_parser.PDF_DOCUMENTS:
+        handle_media(file, folder / 'DOCUMENTS' / 'PDF')
+    for file in file_parser.PPTX_DOCUMENTS:
+        handle_media(file, folder / 'DOCUMENTS' / 'PPTX')
+    
     for file in file_parser.MY_OTHER:
         handle_media(file, folder / 'MY_OTHER')
 
@@ -38,12 +83,13 @@ def main(folder: Path):
         handle_archive(file, folder / 'ARCHIVES')
 
     for folder in file_parser.FOLDERS[::-1]:
-        # Видаляємо пусті папки після сортування
-        try:
-            folder.rmdir()
-        except OSError:
-            print(f'Error during remove folder {folder}')
-
+        if is_empty_directory(folder):
+            try:
+                folder.rmdir()
+            except OSError as e:
+                print(f'Error during remove folder {folder}: {e}')
+        else:
+            remove_directory_contents(folder)
 
 if __name__ == "__main__":
     folder_process = Path(sys.argv[1])
